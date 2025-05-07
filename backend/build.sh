d66a96fa775cd35cfbdf66594fb7976ae6eb3c33
Dasha-collab <darya@pantonepro.ru>
2025-05-07 23:59:34 +0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Установка системных шрифтов
 sudo apt-get update
 sudo apt-get install -y fonts-dejavu
+
+# Установка Python-зависимостей
 pip install -r requirements.txt
+
+# Запуск приложения
 uvicorn main:app --host 0.0.0.0 --port 10000
